6fbfe3be2c582018458c98ec881d90abb76ba69f
MaksymY <maksym.yankivskyypro@gmail.com>
2019-05-15 15:16:12 +0200

    route

---
+++ server.js
@@ -0,0 +1,24 @@
+/*
+Importer les composants serveur
+*/
+  require('dotenv').config();
+  const express = require('express');
+  const path = require('path');
+//
+
+/*
+Configuration du serveur 
+*/
+  // Définir les variables serveur
+  const server = express();
+  const port = process.env.PORT;
+
+  //Définir le dossier static du client 
+  server.set( 'views', __dirname + '/www' );
+  server.use( express.static(path.join(__dirname, 'www')) );
+
+/*
+Lancer le serveur
+*/
+  server.listen( port, () => console.log('Server listening on port ${port}') )
+//
